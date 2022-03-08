ddefa275af5e46ddf743dce0a40f9a07e103e7e8
ajsilva1119 <alexsilva@u.boisestate.edu>
2022-03-08 12:27:26 -0800

    Start to NFA and State

--- fa/nfa/NFA.java
+++ fa/nfa/NFA.java
@@ -3,12 +3,30 @@ package fa.nfa;
 import fa.State;
 import fa.dfa.DFA;
 
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 public class NFA implements fa.nfa.NFAInterface{
+
+    private Set<Character> alphabet;
+    private Set<NFAState> states;
+    private Set<NFAState> finalStates;
+    private NFAState startState;
+
+    public NFA(){
+
+        alphabet = new LinkedHashSet<>();
+        states = new LinkedHashSet<>();
+        finalStates = new LinkedHashSet<>();
+        startState = null;
+
+    }
     @Override
     public void addStartState(String name) {
 
+        NFAState start = new NFAState(name);
+        startState = start;
+        states.add(start);
     }
 
     @Override
--- fa/nfa/NFAState.java
+++ fa/nfa/NFAState.java
@@ -1,5 +1,22 @@
 package fa.nfa;
 
+import java.util.HashMap;
+
 public class NFAState extends fa.State{
 
+    private HashMap<Character,NFAState> transitions;
+    public NFAState(String name) {
+        this.name = name;
+        transitions = new HashMap<Character,NFAState>();
+    }
+
+//    public void getNextState(Character input) {
+//        transitions.get(input);
+//    }
+    public void addTransition(Character input, NFAState newState) {
+        if( !(transitions.containsKey(input))) { //checks if a transition exsits and needs to a make a new spot
+            transitions.put(input, newState);
+        }
+        // need to figure out how to map same key to different state.
+    }
 }
