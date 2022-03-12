26443bebd1fd6f88f0e67201bfbb90236edea1e8
Kincaid Schmitt <kincaidschmitt@u.boisestate.edu>
2022-03-12 12:51:45 -0700

    Finished NFAState class

--- fa/nfa/NFAState.java
+++ fa/nfa/NFAState.java
@@ -1,22 +1,68 @@
 package fa.nfa;
-
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
+/**
+ * Represents an NFAState object. ADD MORE HERE
+ * 
+ * @author Kincaid Schmitt, Alex Silva
+ *
+ */
 public class NFAState extends fa.State{
 
-    private HashMap<Character,NFAState> transitions;
-    public NFAState(String name) {
+    private LinkedHashMap<Character,Set<NFAState>> transitions;
+    private boolean isFinal;
+    
+    /**
+     * Constructor for the NFAState class
+     * 
+     * @param name - name of the state
+     * @param f - is a final state
+     */
+    public NFAState(String name, boolean f) {
         this.name = name;
-        transitions = new HashMap<Character,NFAState>();
+        this.isFinal = f;
+        this.transitions = new LinkedHashMap<Character,Set<NFAState>>();
     }
 
-//    public void getNextState(Character input) {
-//        transitions.get(input);
-//    }
-    public void addTransition(Character input, NFAState newState) {
-        if( !(transitions.containsKey(input))) { //checks if a transition exsits and needs to a make a new spot
+    /**
+     * Gets the set of next states given an input value
+     * 
+     * @param input - transition character
+     * @return Set<NFAState> containing all next states given an input
+     */
+    public Set<NFAState> getNextState(Character input) {
+    	 if( !(transitions.containsKey(input))) {
+    		 return new LinkedHashSet<>();
+    	 }
+    	 return transitions.get(input);
+    }
+    
+    /**
+     * Adds a transition given an input and next state
+     * 
+     * @param input - the transition character
+     * @param tranState - the next state given the transition character
+     */
+    public void addTransition(Character input, NFAState tranState) {
+        if( !(transitions.containsKey(input))) { //checks if a transition exists and needs to a make a new spot
+        	Set<NFAState> newState = new LinkedHashSet<>();
+        	newState.add(tranState);
             transitions.put(input, newState);
+        } else {
+        	Set<NFAState> states = transitions.get(input); //get the current set of transitions for the input
+        	states.add(tranState); //add the new state to the list
         }
-        // need to figure out how to map same key to different state.
+    }
+    
+    /**
+     * Method to check if the state is a final state 
+     * 
+     * @return true if the state is a final state
+     */
+    public boolean isFinalState() {
+    	return this.isFinal;
     }
 }
